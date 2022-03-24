96238457f89e9025259813fad411de041c129014
simransdsu <simran5590@gmail.com>
2022-03-24 14:22:42 +0530

    Adding the error message label

--- Bankey/LoginViewController.swift
+++ Bankey/LoginViewController.swift
@@ -11,6 +11,7 @@ class LoginViewController: UIViewController {
     
     private let loginView = LoginView()
     private let signInButton = UIButton(type: .system)
+    private let errorMessageLabel = UILabel()
     
     override func viewDidLoad() {
         
@@ -25,17 +26,27 @@ extension LoginViewController {
     private func style() {
         
         loginView.translatesAutoresizingMaskIntoConstraints = false
+        
         signInButton.translatesAutoresizingMaskIntoConstraints = false
         signInButton.configuration = .filled()
         signInButton.configuration?.imagePadding = 8
         signInButton.setTitle("Sign In", for: .normal)
         signInButton.addTarget(self, action: #selector(signInTapped), for: .primaryActionTriggered)
+        
+        errorMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        errorMessageLabel.numberOfLines = 0
+//        errorMessageLabel.isHidden = true
+        errorMessageLabel.textAlignment = .center
+        errorMessageLabel.textColor = .systemRed
+        errorMessageLabel.text = "Error Failure"
+        
     }
     
     private func layout() {
         
         view.addSubview(loginView)
         view.addSubview(signInButton)
+        view.addSubview(errorMessageLabel)
         NSLayoutConstraint.activate([
             loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
@@ -44,6 +55,10 @@ extension LoginViewController {
             signInButton.topAnchor.constraint(equalTo: loginView.bottomAnchor, constant: 16),
             signInButton.leadingAnchor.constraint(equalTo: loginView.leadingAnchor),
             signInButton.trailingAnchor.constraint(equalTo: loginView.trailingAnchor),
+            
+            errorMessageLabel.topAnchor.constraint(equalTo: signInButton.bottomAnchor, constant: 16),
+            errorMessageLabel.leadingAnchor.constraint(equalTo: signInButton.leadingAnchor),
+            errorMessageLabel.trailingAnchor.constraint(equalTo: signInButton.trailingAnchor)
         ])
     }
 }
